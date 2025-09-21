d3e615253365514d3e3e88ea9ca214082c3188ac
ella4719 <rhdmstn0424@gmail.com>
2025-09-21 21:32:14 +0900

    feat: 스웨거용 엔드포인트 대시(/)버전도 열어주기

--- src/app.js
+++ src/app.js
@@ -42,10 +42,24 @@ app.use(helmet({
   },
 }));
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: process.env.CLIENT_URL || 'http://localhost:3000',
+//   credentials: true,
+// }));
+
+const whitelist = [
+    'http://localhost:3000',
+    'http://43.201.82.124',
+    'http://ec2-43-201-82-124.ap-northeast-2.compute.amazonaws.com',
+    'https://localhost:3000',
+    'https://43.201.82.124',
+    'https://ec2-43-201-82-124.ap-northeast-2.compute.amazonaws.com',
+    'https://waayto.com',  // 배포 도메인 생기면 추가
+    'http://waayto.com',
+    'https://waytomeet.site',
+    'http://waytomeet.site'
+];
+app.use(cors({ origin: whitelist, credentials: true }));
 
 // Rate limiting을 API 라우트에만 적용
 app.use('/api', limiter);
